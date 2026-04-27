2c4e09478e0e22b9783941016b63c5e28efbefb9
pooreumjung <pooreumjung02@naver.com>
2026-04-27 22:35:54 +0900

    chore: SwaggerConfig 설정

---
+++ src/main/java/com/ssupick/ssupick_be/common/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.ssupick.ssupick_be.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SSUPICK Backend API")
+                        .description("SSUPICK 프로젝트 백엔드 API 문서")
+                        .version("1.0.0")
+                );
+    }
+
+}
