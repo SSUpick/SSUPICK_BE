dd702ed939483656ffad6ef77f534fce8461ca9b
pooreumjung <pooreumjung02@naver.com>
2026-04-27 21:59:05 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/ssupick/ssupick_be/SsupickBeApplication.java
@@ -0,0 +1,12 @@
+package com.ssupick.ssupick_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsupickBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsupickBeApplication.class, args);
+    }
+}
