44dfcc834d21558d0e1611671daf3d8c7f3b6e1f
pooreumjung <pooreumjung02@naver.com>
2026-05-07 21:14:52 +0900

    refactor: RegisterUserOnboarding Validation 값 수정

--- src/main/java/com/ssupick/ssupick_be/domain/user/dto/request/UpdateUserProfileRequest.java
+++ src/main/java/com/ssupick/ssupick_be/domain/user/dto/request/UpdateUserProfileRequest.java
@@ -13,7 +13,7 @@ public record UpdateUserProfileRequest(
 
         @Schema(description = "닉네임", example = "숭실대 카리나")
         @NotBlank(message = "닉네임은 필수입니다.")
-        @Size(max = 10, message = "닉네임은 20자 이하로 입력해주세요.")
+        @Size(max = 10, message = "닉네임은 10자 이하로 입력해주세요.")
         String nickname,
 
         @Schema(description = "MBTI", example = "INTJ")
